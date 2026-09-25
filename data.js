8cd838dcca1b7dc484050712b8b256a6e6de6d89
busian040-creator <busian040@gmail.com>
2026-09-25 17:46:26 +0300

    Update data.js
    Data JavaScript created

--- data.js
+++ data.js
@@ -1,1 +1,7 @@
 
+window.BUSIAN={products:[
+{id:1,n:'Samsung 55\" Smart TV',m:'Busia Home Electronics',p:75000,c:'Electronics',i:'📺'},
+{id:2,n:'Chicken & Chips',m:'Town Kitchen',p:450,c:'Food',i:'🍗'},
+{id:3,n:'5kg Premium Rice',m:'Busia Fresh Mart',p:780,c:'Groceries',i:'🍚'},
+{id:4,n:'Classic Sneakers',m:'Western Styles',p:2500,c:'Fashion',i:'👟'}
+],categories:['Food & Restaurants','Groceries','Electronics','Fashion','Beauty','Cakes & Events','Local Shops','Services']};
